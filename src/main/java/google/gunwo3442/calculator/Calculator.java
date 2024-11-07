7c1e04b6bc0d0929b0b0a9c58fbb17b6f8841ae3
gunwoda <gunwo3442@gmail.com>
2024-11-07 16:35:50 +0900

    jenkins update

---
+++ src/main/java/google/gunwo3442/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package google.gunwo3442.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b){
+        return a + b;
+    }
+}
